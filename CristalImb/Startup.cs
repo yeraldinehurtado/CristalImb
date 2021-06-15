eb9fe8676a97722a85a7e689f786a6c65367763a
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-06-15 15:01:26 -0500

    Agregué plantilla para index

--- CristalImb/Startup.cs
+++ CristalImb/Startup.cs
@@ -50,7 +50,7 @@ namespace CristalImb
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Inicio}/{action=Index}/{id?}");
             });
         }
     }
