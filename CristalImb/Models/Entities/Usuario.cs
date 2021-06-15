e28f6d6d413f77e4e94f93bfb30e1f84f70bc8fd
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-06-15 18:09:42 -0500

    Entidad usuario

--- CristalImb/Models/Entities/Usuario.cs
+++ CristalImb/Models/Entities/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,25 @@ namespace CristalImb.Models.Entities
 {
     public class Usuario
     {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        [DisplayName("Nombre de usuario")]
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
+        public string NombreUsuario { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "Debe elegir un rol")]
+        public int Rol { get; set; }
+
+        [DisplayName("Contraseña")]
+        [Required(ErrorMessage = "Debe escribir una contraseña")]
+        public string Contrasena { get; set; }
+
+        [DisplayName("Confirmar contraseña")]
+        [Required(ErrorMessage = "Debe confirmar la contraseña")]
+        public string ConfirmarContrasena { get; set; }
     }
 }
