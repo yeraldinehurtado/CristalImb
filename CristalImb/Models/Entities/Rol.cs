1f1ca89788fc9069054ca2378fd79908d9b7457e
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-06-15 14:40:35 -0500

    Cree las propiedades de rol

--- CristalImb/Models/Entities/Rol.cs
+++ CristalImb/Models/Entities/Rol.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,19 @@ namespace CristalImb.Models.Entities
 {
     public class Rol
     {
+        [Key]
+        public int RolId { get; set; }
+
+        [DisplayName("Nombre rol")]
+        [Required(ErrorMessage = "El nombre del rol es obligatorio")]
+        public string NombreRol { get; set; }
+
+        [DisplayName("Módulo")]
+        [Required(ErrorMessage = "El módulo es obligatorio")]
+        public string Modulo { get; set; }
+
+        public int Permisos { get; set; }
+        public string Estado { get; set; }
+
     }
 }
