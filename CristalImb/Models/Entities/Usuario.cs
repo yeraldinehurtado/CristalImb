ef1627b14b17c4aa7b6a26d5df4d64747d186ca0
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-06-15 14:14:11 -0500

    Cree clases en entidades

---
+++ CristalImb/Models/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CristalImb.Models.Entities
+{
+    public class Usuario
+    {
+    }
+}
