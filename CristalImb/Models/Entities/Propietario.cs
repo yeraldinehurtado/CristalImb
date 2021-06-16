d5e560195871435626e5768714f0adb60ab5d82a
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-06-15 21:24:18 -0500

    Entidad propietario

--- CristalImb/Models/Entities/Propietario.cs
+++ CristalImb/Models/Entities/Propietario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,21 @@ namespace CristalImb.Models.Entities
 {
     public class Propietario
     {
+        [Key]
+        public int PropietarioId { get; set; }
+        
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        public string Apellido { get; set; }
+
+        [DisplayName("Teléfono")]
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        public int Telefono { get; set; }
+
+        [DisplayName("Dirección")]
+        [Required(ErrorMessage = "La dirección es obligatoria")]
+        public string Direccion { get; set; }
     }
 }
