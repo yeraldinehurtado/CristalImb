1251c9668de7c4f3b46f4be1f7b69b091c7c97a9
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-06-26 03:00:47 -0500

    Hice index de roles y crear roles

--- CristalImb.Web/Controllers/RolController.cs
+++ CristalImb.Web/Controllers/RolController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CristalImb.Model.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,33 @@ namespace CristalImb.Web.Controllers
 {
     public class RolController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<UsuarioIdentity> _userManager;
+        private readonly SignInManager<UsuarioIdentity> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolController(UserManager<UsuarioIdentity> userManager, SignInManager<UsuarioIdentity> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult IndexRol()
+        {
+            var listRoles = _roleManager.Roles.ToList();
+            return View(listRoles);
+        }
+
+        public IActionResult CrearRol()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearRol(string rol)
+        {
+            await _roleManager.CreateAsync(new IdentityRole(rol));
+            return RedirectToAction(nameof(IndexRol));
+        }
+
     }
 }
