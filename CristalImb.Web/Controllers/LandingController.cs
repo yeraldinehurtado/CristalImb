70c2a5511798efcbf756a27f49b142367bbeddf5
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-11-13 20:20:47 -0500

    .

---
+++ CristalImb.Web/Controllers/LandingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CristalImb.Web.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult IndexLanding()
+        {
+            return View();
+        }
+    }
+}
