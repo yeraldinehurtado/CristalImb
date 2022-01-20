2453c3d0db9c222f0b24ac4803ffa500ce9e3ad9
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2022-01-20 13:42:00 -0500

    inmuebles en venta y arriendo en landing

--- CristalImb.Web/Controllers/LandingController.cs
+++ CristalImb.Web/Controllers/LandingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CristalImb.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,39 @@ namespace CristalImb.Web.Controllers
 {
     public class LandingController : Controller
     {
+        private readonly IInmuebleService _inmuebleService;
+        private readonly ITipoInmuebleService _tipoInmuebleService;
+        private readonly IZonaService _zonaService;
+        private readonly IServiciosInmuebleService _serviciosInmuebleService;
+
+        public LandingController(IInmuebleService inmuebleService, ITipoInmuebleService tipoInmuebleService, IZonaService zonaService, IServiciosInmuebleService serviciosInmuebleService)
+        {
+            _inmuebleService = inmuebleService;
+            _tipoInmuebleService = tipoInmuebleService;
+            _zonaService = zonaService;
+            _serviciosInmuebleService = serviciosInmuebleService;
+        }
+
+        //landing inicio
         public IActionResult IndexLanding()
         {
             return View();
+        }
+
+        //landing inmuebles en venta
+        [HttpGet]
+        public async Task<IActionResult> InmueblesVenta()
+        {
+             return View(await _inmuebleService.ObtenerListaInmueblesEstado());
+
+        }
+
+        //inmuebles en arriendo
+        [HttpGet]
+        public async Task<IActionResult> InmueblesArriendo()
+        {
+            return View(await _inmuebleService.ObtenerListaInmueblesEstado());
+
         }
     }
 }
