b0fe3f6d9d6b093b2a0beb4ea1afea77788bc483
Sena <Sena@DESKTOP-73BD9ED>
2022-02-18 14:57:39 -0500

    .

--- CristalImb.Web/wwwroot/js/menu.js
+++ CristalImb.Web/wwwroot/js/menu.js
@@ -33,3 +33,17 @@
 		window.location.href = $(this).attr("href");
 	});
 });
+
+
+$("#number").on({
+	"focus": function (event) {
+		$(event.target).select();
+	},
+	"keyup": function (event) {
+		$(event.target).val(function (index, value) {
+			return value.replace(/\D/g, "")
+				.replace(/([0-9])([0-9]{3})$/, '$1.$2')
+				.replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ",");
+		});
+    }
+});
