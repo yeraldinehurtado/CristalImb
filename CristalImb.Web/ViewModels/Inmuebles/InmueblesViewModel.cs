ebb5547811790faf267fe336b24c5770e84e58c0
Sena <Sena@DESKTOP-73BD9ED>
2022-02-18 12:54:07 -0500

    .

--- CristalImb.Web/ViewModels/Inmuebles/InmueblesViewModel.cs
+++ CristalImb.Web/ViewModels/Inmuebles/InmueblesViewModel.cs
@@ -39,9 +39,9 @@ namespace CristalImb.Web.ViewModels.Inmuebles
         public int ServicioInmuebleId { get; set; }
 
         [Required(ErrorMessage = "El valor es obligatorio")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Valor incorrecto")]
         [UIHint("Currency")]
         [DisplayFormat(DataFormatString ="{0:C0}")]
-        [RegularExpression("^\\d+$", ErrorMessage = "Valor incorrecto")]
         [Range(1, 9999999999)]
         public long Valor { get; set; }
 
